c0437b9575fb68d298ab0f2d39a05314fd2df36e
manohySr <manohyrajaonah@gmail.com>
2024-09-11 12:46:39 +0400

    feat:	adding audio streaming

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,28 @@
-import { serve } from '@hono/node-server'
-import { Hono } from 'hono'
+import { serve } from "@hono/node-server";
+import { Hono } from "hono";
+import { stream } from "hono/streaming";
+import fs from "fs";
+import path from "path";
+import { Readable } from "stream";
 
-const app = new Hono()
+const app = new Hono();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.get("/", (c) => {
+  return c.text("Hello test!");
+});
 
-const port = 3000
-console.log(`Server is running on port ${port}`)
+app.get("/audio", (c) => {
+  const audioFile = path.join(__dirname, "../", "static", "audio.mp3");
+  const fileStream = fs.createReadStream(audioFile);
+
+  // Return the readStream as the response body => like readStream.pipe(res) for nodejs
+  return c.body(fileStream);
+});
+
+const port = 3000;
+console.log(`Server is running on port ${port}`);
 
 serve({
   fetch: app.fetch,
-  port
-})
+  port,
+});
